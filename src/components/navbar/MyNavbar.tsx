96d5ab1462255679f82f0ff443fbbc9db1a70ced
AllenBastian <reachallenbastian@gmail.com>
2024-08-19 19:39:00 +0530

    ADDED: logout

--- src/components/navbar/MyNavbar.tsx
+++ src/components/navbar/MyNavbar.tsx
@@ -1,32 +1,46 @@
-
+"use client";
 import React from "react";
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@nextui-org/react";
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+} from "@nextui-org/react";
 import { MyButton } from "../buttons/mybutton";
 import { Spinner } from "@nextui-org/react";
 import { ThemeSwitcher } from "../themes/themeSwitcher";
-import Link from "next/link"; // Import Link from next/link
+import Link from "next/link";
+import { myInstance } from "@/utils/Axios/axios";
+import { useRouter } from "next/navigation";
+import { toast } from "sonner";
 
 export default function MyNavbar() {
+
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    try {
+      const response = await myInstance.post("/auth/logout");
+      toast.success(response.data.message);
+      router.push("/login");
+
+    } catch (error: any) {
+      console.log(error);
+      toast.error(error.response?.data.message || "An error occurred");
+    }
+  };
   return (
     <Navbar className="justify-center" maxWidth="full">
-      <NavbarBrand>
-        {/* Add content or logo here if needed */}
-      </NavbarBrand>
+      <NavbarBrand></NavbarBrand>
       <NavbarContent className="hidden sm:flex gap-12" justify="end">
         <NavbarItem>
-          <Link href="/credaegis/organization/dashboard" >
-            dasboard
-          </Link>
+          <Link href="/credaegis/organization/dashboard">dasboard</Link>
         </NavbarItem>
         <NavbarItem>
-          <Link href="/login" >
-            login
-          </Link>
+          <Link href="/login">login</Link>
         </NavbarItem>
         <NavbarItem>
-          <Link href="#">
-            
-          </Link>
+          <Link href="#"></Link>
         </NavbarItem>
       </NavbarContent>
       <NavbarContent justify="end" className="gap-4">
@@ -39,6 +53,9 @@ export default function MyNavbar() {
             size="sm"
             spinner={<Spinner size="sm" color="default" />}
             color="default"
+            onClick={() => {
+              handleLogout();
+            }}
           >
             <span className="dark:text-black text-white text-md font-medium">
               Logout
