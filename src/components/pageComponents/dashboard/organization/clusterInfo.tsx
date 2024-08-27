2964b8389677852027df1076305fd9e34ab65ee4
AllenBastian <reachallenbastian@gmail.com>
2024-08-27 21:57:15 +0530

    FIXED: responsive iteration 2 dashboard

--- src/components/pageComponents/dashboard/organization/clusterInfo.tsx
+++ src/components/pageComponents/dashboard/organization/clusterInfo.tsx
@@ -70,16 +70,16 @@ const ClusterInfo: React.FC<ClusterInfoProps> = ({
             fetchClusterInfo={fetchClusterInfo}
           />
         );
-        case "changeAdmin":
-            return (
-                <ChangeAdmin
-                admin_ulid={admin.admin_ulid}
-                setIsOpen={setIsOpen}
-                cluster_ulid={cluster.cluster_ulid}
-                fetchClusterInfo={fetchClusterInfo}
-                members={cluster.membersInfo}
-                />
-            );
+      case "changeAdmin":
+        return (
+          <ChangeAdmin
+            admin_ulid={admin.admin_ulid}
+            setIsOpen={setIsOpen}
+            cluster_ulid={cluster.cluster_ulid}
+            fetchClusterInfo={fetchClusterInfo}
+            members={cluster.membersInfo}
+          />
+        );
 
       default:
         return null;
@@ -100,7 +100,102 @@ const ClusterInfo: React.FC<ClusterInfoProps> = ({
   };
 
   return (
-    <div className="h-full p-4 rounded-lg shadow-sm flex flex-col lg:flex-row lg:items-center justify-between lg:gap-8">
+    <div className="h-full p-3 rounded-lg shadow-sm ">
+      <div className="grid grid-cols-1 lg:grid-cols-9 gap-5 h-full">
+        <div className="col-span-4">
+          <div className="grid grid-rows-2 h-full gap-3">
+            <div className="row-span-1">
+              <div className="flex items-center gap-4">
+                <MdInfo size={32} className="text-blue-500" />
+                <div>
+                  <h1 className="text-xl font-bold text-gray-900 dark:text-white">
+                    {cluster.cluster_name}
+                  </h1>
+                  <p className="text-sm text-gray-500 dark:text-gray-400">
+                    Created at: {new Date(cluster.created_at).toLocaleString()}
+                  </p>
+                </div>
+            </div>
+
+            </div>
+            <div className="row-span-1">
+
+            <div className="flex  lg:justify-start justify-end lg:ml-12  flex-row lg:items-center gap-2 mt-4 lg:mt-0">
+              {cluster.deactivated === 0 ? (
+                <MyButton
+                  className="bg-black dark:bg-white"
+                  size="sm"
+                  onClick={() => handleOpenModal("deactivate")}
+                >
+                  <span className="dark:text-black text-white text-md font-medium">
+                    Deactivate
+                  </span>
+                </MyButton>
+              ) : (
+                <MyButton
+                  className="bg-black dark:bg-white"
+                  size="sm"
+                  onClick={() => {
+                    handleActivateCluster();
+                  }}
+                >
+                  <span className="dark:text-black text-white text-md font-medium">
+                    Activate
+                  </span>
+                </MyButton>
+              )}
+              <MyButton
+                className="bg-black dark:bg-white "
+                size="sm"
+                onClick={() => handleOpenModal("delete")}
+              >
+                <span className="dark:text-black text-white text-md font-medium">
+                  Delete
+                </span>
+              </MyButton>
+              </div>
+               
+              </div>
+          </div>
+        </div>
+        <div className="lg:hidden border-t border-gray-300 dark:border-stone-600 mt-5 mb-5"></div>
+        <div className="border-l border-gray-300 dark:border-stone-600 mb-5 hidden lg:block"></div>
+        <div className="col-span-4">
+        
+          <div className="grid grid-rows-2 gap-4">
+            <div className="row-span-1">
+              <div className="flex items-center gap-4">
+                <FaUserShield size={32} className="text-green-500" />  
+                <div>
+                  <h2 className="text-xl font-semibold text-gray-900 dark:text-white">
+                    {admin?.admin_name || "N/A"}
+                  </h2>
+                  <p className="text-sm text-gray-600 dark:text-gray-400">
+                    {admin?.admin_email || "N/A"}
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="row-span-1">
+              <div className="flex lg:justify-start justify-end lg:ml-12 mt-4 lg:mt-0">
+                <MyButton
+                  className="bg-black dark:bg-white"
+                  size="sm"
+                  onClick={() => handleOpenModal("changeAdmin")}
+                >
+                  <span className="dark:text-black text-white text-md font-medium">
+                    Change Admin
+                  </span>
+                </MyButton>
+              </div>
+              </div>
+          </div>
+          
+
+        </div>
+        
+      </div>
+
       {/* <div className="flex flex-col lg:flex-row lg:items-center lg:gap-8 w-full">
         <div className="flex items-center gap-4">
         <div className="flex items-center gap-4">
