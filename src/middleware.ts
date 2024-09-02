972b220c76c063406b17311eda4da8053ccf6933
AllenBastian <reachallenbastian@gmail.com>
2024-09-02 12:50:15 +0530

    MODIFIED: removed middle ware

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,12 +6,14 @@ import authenticator from './utils/Auth/authenticator'
 
 export async function middleware (request: NextRequest) {
      
-    const isAuthenticated = await authenticator();
-    console.log("is true?",isAuthenticated);
-    if(!isAuthenticated)
-        return NextResponse.redirect(new URL('/login', request.url));
-    else
-        return NextResponse.next();
+    // const isAuthenticated = await authenticator();
+    // console.log("is true?",isAuthenticated);
+    // if(!isAuthenticated)
+    //     return NextResponse.redirect(new URL('/login', request.url));
+    // else
+    //     return NextResponse.next();
+
+    return NextResponse.next();
 
         
 }
