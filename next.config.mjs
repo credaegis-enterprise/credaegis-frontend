9168e67cf5b75cd0c8ec446bdaedea50270313cf
AllenBastian <reachallenbastian@gmail.com>
2024-09-02 23:29:43 +0530

    FIXED:?

--- next.config.mjs
+++ next.config.mjs
@@ -6,7 +6,7 @@ const nextConfig = {
         return [
             {
                 source: '/api/:path*',
-                destination: `${baseUrl}/:path*`,
+                destination: `${baseUrl}/api/:path*`,
             },
         ];
     },
