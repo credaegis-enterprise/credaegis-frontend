0cc6155582ea24f8df5b36faad74ce2f40885c7e
AllenBastian <reachallenbastian@gmail.com>
2024-09-13 01:56:28 +0530

    ADDED: file upload refined

--- src/components/pageComponents/certificates/myFileList.tsx
+++ src/components/pageComponents/certificates/myFileList.tsx
@@ -5,6 +5,8 @@ import { HiDocumentText } from "react-icons/hi";
 import { Select, SelectItem } from "@nextui-org/react";
 import { motion } from "framer-motion";
 import { MyButton } from "@/components/buttons/mybutton";
+import { MdClose } from "react-icons/md";
+import { toast } from "sonner";
 
 const MyFileList = () => {
   const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
@@ -17,9 +19,22 @@ const MyFileList = () => {
   const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     if (e.target.files) {
       const filesArray = Array.from(e.target.files);
+        if (filesArray.length > 10 || selectedFiles.length + filesArray.length > 10) {
+
+            filesArray.splice(10, filesArray.length - 10);
+            toast.warning("You can only upload a maximum of 10 files at a time");
+        
+        }
       setSelectedFiles((prev) => [...prev, ...filesArray]);
     }
   };
+
+
+  const handleFileRemove = (index: number) => {
+    const newFiles = [...selectedFiles];
+    newFiles.splice(index, 1);
+    setSelectedFiles(newFiles);
+  }
 
   console.log(selectedFiles);
   return (
@@ -28,8 +43,11 @@ const MyFileList = () => {
         <div className="flex gap-2 mt-1 ">
           <HiDocumentText size={26} />
           <div className="text-xl font-semibold text-gray-800 dark:text-gray-200">
-            Certificates
+            Certificates   
           </div>
+          <div className="ml-3 text-xl font-medium text-gray-800 dark:text-gray-200">
+            {selectedFiles.length}/10
+            </div>
         </div>
         <input
           type="file"
@@ -67,37 +85,44 @@ const MyFileList = () => {
       </div>
       <div className=" mt-2  h-full  overflow-auto">
         <div className="space-y-2 p-2 mt-1 h-full">
-          {!selectedFiles? (
+          {selectedFiles.length === 0 ? (
             <div className="flex h-full justify-center items-center text-lg">
               No files uploaded.
             </div>
           ) : (
-         
-          
-          selectedFiles && selectedFiles.map((file, index) => (
-            <motion.div
-              key={index}
-              initial={{ opacity: 0, y: 20 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.3, delay: index * 0.1 }}
-              whileHover={{
-                scale: 1.01,
-                boxShadow: "0px 10px 20px rgba(0, 0, 0, 0.1)",
-              }}
-              whileTap={{ scale: 0.98 }}
-              className="bg-white dark:bg-stone-900 rounded-lg shadow-sm p-4 transition-colors duration-300 hover:bg-gray-100 dark:hover:bg-stone-700 cursor-pointer"
-            >
-              <h3 className="text-sm font-medium text-gray-900 dark:text-gray-100">
-                {file.name}
-              </h3>
-            </motion.div>
-          ))
-      
+            selectedFiles &&
+            selectedFiles.map((file, index) => (
+              <motion.div
+                key={index}
+                initial={{ opacity: 0, y: 20 }}
+                animate={{ opacity: 1, y: 0 }}
+                transition={{ duration: 0.3, delay: index * 0.1 }}
+                whileHover={{
+                  scale: 1.01,
+                  boxShadow: "0px 10px 20px rgba(0, 0, 0, 0.1)",
+                }}
+                whileTap={{ scale: 0.98 }}
+                className="bg-white dark:bg-stone-900 rounded-lg shadow-sm p-4 transition-colors duration-300 hover:bg-gray-100 dark:hover:bg-stone-700 cursor-pointer"
+              >
+                <div className="flex justify-between">
+                  <div className="text-sm font-medium text-gray-900 dark:text-gray-100">
+                    {file.name}
+                  </div>
+                  <div className="group">
+                    <div className="flex items-center justify-center h-8 w-8 rounded-full transition-colors duration-300 group-hover:bg-gray-200 dark:group-hover:bg-red-700">
+                      <MdClose
+                        size={20}
+                        className="text-white group-hover:text-black dark:group-hover:text-white"
+                        onClick={() => handleFileRemove(index)}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </motion.div>
+            ))
           )}
         </div>
-
-      
-    </div>
+      </div>
     </div>
   );
 };
