499ec137c3ea0c301fc92cc29bb5bebc373efd86
AllenBastian <reachallenbastian@gmail.com>
2024-09-16 16:55:37 +0530

    ADDED: layout fixed

---
+++ src/components/pageComponents/certificates/approveCertificates/approvalControl.tsx
@@ -0,0 +1,12 @@
+const ApprovalControl = ()=>{
+    return(
+
+        <div className="h-full  mt-2">
+        <div className="flex flex-col w-full">
+            sknkcmn
+            </div>
+            </div>
+    )
+}
+
+export default ApprovalControl;
--- src/components/pageComponents/certificates/approveCertificates/approveCertifcates.tsx
+++ src/components/pageComponents/certificates/approveCertificates/approveCertifcates.tsx
@@ -1,67 +1,113 @@
 import { MyButton } from "@/components/buttons/mybutton";
 import { Checkbox } from "@nextui-org/react";
 
-
 const ApproveCertificates = () => {
-    const certificates = [
-        {
-            eventName: "Tech Conference 2024 sckskjcbjkbkjbcjkkjsbskjbxkjsbxjknjksxjkskbsxjkbxsjbxsbkjxbkjsbkjbxskbxjkbskjxkjxbkjsbxjkbxkjsbkjsbxkjsxkjbxsjkbxskjbxksbxkjxbsjkxbjksbxkjsbxkjsbxjk",
-            issuedTo: "John Doe",
-            uploadedBy: "Admin",
-            expiry: "2025-01-01",
-            comments: "Pending review",
-        },
-        {
-            eventName: "Tech Conference 2024",
-            issuedTo: "John Doe",
-            uploadedBy: "Admin",
-            expiry: "2025-01-01",
-            comments: "Pending review",
-        },
+  const certificates = [
+    {
+      eventName: "Tech Conference 2024 ",
+      issuedTo: "John Does",
+      uploadedBy: "Admin",
+      expiry: "2025-01-01",
+      comments: "My certificate",
+    },
+    {
+      eventName: "Tech Conference 2024",
+      issuedTo: "John Doe",
+      uploadedBy: "Admin",
+      expiry: "2025-01-01",
+      comments: "Pending review",
+    },
+    {
+        eventName: "Tech Conference 2024 ",
+        issuedTo: "John Does",
+        uploadedBy: "Admin",
+        expiry: "2025-01-01",
+        comments: "My certificate",
+      },
+      {
+        eventName: "Tech Conference 2024",
+        issuedTo: "John Doe",
+        uploadedBy: "Admin",
+        expiry: "2025-01-01",
+        comments: "Pending review",
+      },
       
-       
+  ];
 
-    ];
-
-    return (
-        
-        <div className="h-full border dark:border-stone-800 rounded-lg p-4 w-full">
-        <div className="h-full overflow-x-auto w-full">
-            <div className="overflow-y-auto h-full">
-                <table className="min-w-full table-fixed">
-            
-                    <thead className="bg-gray-100 dark:bg-stone-900 sticky top-0 z-30">
-                        <tr>
-                            <th className="px-4 py-2 text-left">Event Name</th>
-                            <th className="px-4 py-2 text-left">Issued To</th>
-                            <th className="px-4 py-2 text-left">Uploaded By</th>
-                            <th className="px-4 py-2 text-left">Expiry</th>
-                            <th className="px-4 py-2 text-left">Comments</th>
-                            <th className="px-4 py-2 text-center w-12 "></th>
-    
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {certificates.map((certificate, index) => (
-                            <tr key={index} className="border-b dark:border-stone-800  ">
-                                <td className="px-4 py-2 break-words">{certificate.eventName}</td>
-                                <td className="px-4 py-2 break-words">{certificate.issuedTo}</td>
-                                <td className="px-4 py-2 break-words">{certificate.uploadedBy}</td>
-                                <td className="px-4 py-2">{certificate.expiry}</td>
-                                <td className="px-4 py-2">{certificate.comments}</td>
-                                <td className="px-4 py-2 flex">
-                                    <Checkbox />
-                                    </td>
-                    
-                            </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        </div>
+  return (
+    <div className="h-full flex flex-col">
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg border dark:border-neutral-800">
+      <table className="w-full text-sm text-left">
+        <thead className="text-md bg-neutral-100 dark:bg-neutral-800 rounded-t-lg">
+          <tr>
+            <th
+              scope="col"
+              className="px-6 py-3 text-neutral-700 dark:text-neutral-200 font-semibold"
+            >
+              Event Name
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-neutral-700 dark:text-neutral-200 font-semibold"
+            >
+              Issued To
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-neutral-700 dark:text-neutral-200 font-semibold"
+            >
+              Uploaded By
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-neutral-700 dark:text-neutral-200 font-semibold"
+            >
+              Expiry
+            </th>
+            <th
+              scope="col"
+              className="px-6 py-3 text-neutral-700 dark:text-neutral-200 font-semibold"
+            >
+              Comments
+            </th>
+            <th scope="col" className="px-6 py-3 text-center w-12"></th>
+          </tr>
+        </thead>
+        <tbody>
+          {certificates.map((certificate, index) => (
+            <tr key={index} className="">
+              <td className="px-6 py-4 text-neutral-900 dark:text-neutral-100">
+                {certificate.eventName}
+              </td>
+              <td className="px-6 py-4 text-neutral-900 dark:text-neutral-100">
+                {certificate.issuedTo}
+              </td>
+              <td className="px-6 py-4 text-neutral-900 dark:text-neutral-100">
+                {certificate.uploadedBy}
+              </td>
+              <td className="px-6 py-4 text-neutral-900 dark:text-neutral-100">
+                {certificate.expiry}
+              </td>
+              <td className="px-6 py-4 text-neutral-900 dark:text-neutral-100">
+                {certificate.comments}
+              </td>
+              <td className="px-6 py-4 text-center">
+                <div className="flex justify-center gap-4">
+                <MyButton size="sm" className="bg-black dark:bg-white">
+                    <span className="dark:text-black text-white text-md font-medium">
+                        View
+                    </span>
+                </MyButton>
+                <Checkbox className="form-checkbox text-neutral-600 dark:text-neutral-300" color="success" />
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-    
-    );
+    </div>
+  );
 };
 
 export default ApproveCertificates;
