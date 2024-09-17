59ccfb3b159473bd1fa9f045e47f097fd6fdd1d5
AllenBastian <reachallenbastian@gmail.com>
2024-09-17 13:11:45 +0530

    ADDED: debouncing search

--- src/components/pageComponents/certificates/approveCertificates/approvalControl.tsx
+++ src/components/pageComponents/certificates/approveCertificates/approvalControl.tsx
@@ -1,132 +1,115 @@
-import { Autocomplete,AutocompleteItem } from "@nextui-org/react";
+import { Autocomplete, AutocompleteItem } from "@nextui-org/react";
 import { MyButton } from "@/components/buttons/mybutton";
 import { MdSearch } from "react-icons/md";
+import { debounce } from "lodash";
 
 import { useState } from "react";
 import { myInstance } from "@/utils/Axios/axios";
 
-
-interface ApproveCertificatesProps {
-
-   
-    }
+interface ApproveCertificatesProps {}
 
 interface clusterType {
-    cluster_name: string;
-    cluster_ulid: string;
+  cluster_name: string;
+  cluster_ulid: string;
 }
 
 interface eventType {
-    event_name: string;
-    event_ulid: string;
-    cluster_ulid: string;
+  event_name: string;
+  event_ulid: string;
+  cluster_ulid: string;
 }
 
+const ApprovalControl: React.FC<ApproveCertificatesProps> = ({}) => {
+  const [clusterList, setClusterList] = useState<clusterType[]>([]);
+  const [eventList, setEventList] = useState<eventType[]>([]);
 
+  const debouncedSearchClusters = debounce(async (value: string) => {
+    try {
+      const response = await myInstance.get(
+        `/search/cluster?cluster_name=${value}`
+      );
 
-const ApprovalControl: React.FC<ApproveCertificatesProps> = ({ }) => {
-
-    const [clusterList, setClusterList] = useState<clusterType[]>([]);
-    const [eventList, setEventList] = useState<eventType[]>([]);
-
-
-    const searchClusters = async (value:string) => {
-        console.log(value);
-        try{
-            const response = await myInstance.get(`/search/cluster?cluster_name=${value}`);
-            console.log(response.data.data);
-            setClusterList(response.data.data);
-            
-        }
-        catch(err){
-            console.log(err);
-        }
+      setClusterList(response.data.data);
+    } catch (err) {
+      console.log(err);
     }
+  }, 300);
 
-    const searchEvents = async (value:string) => {
-        console.log(value);
-        try{
-            const response = await myInstance.get(`/search/event?event_name=${value}&cluster_ulid=`);
-            console.log(response.data.data);
-            setEventList(response.data.data);
-            
-        }
-        catch(err){
-            console.log(err);
-        }
+  const debouncedSearchEvents = debounce(async (value: string) => {
+    try {
+      const response = await myInstance.get(
+        `/search/event?event_name=${value}`
+      );
+
+      setEventList(response.data.data);
+    } catch (err) {
+      console.log(err);
     }
+  }, 300);
 
-    
+  const searchClusters = async (value: string) => {
+    debouncedSearchClusters(value);
+  };
+
+  const searchEvents = async (value: string) => {
+    debouncedSearchEvents(value);
+  };
 
   return (
     <div className="flex flex-col border dark:border-stone-800 border-gray-200 mb-2 rounded-lg p-2 ">
-        <div className="flex text-lg font-medium ml-2">
-            <MdSearch size={26} />
-            <div>
-              Search and Filter
-            </div>
-        
-        </div>
-        <div className="flex flex-col lg:flex-row  gap-4 p-2">
+      <div className="flex text-lg font-medium ml-2">
+        <MdSearch size={26} />
+        <div>Search and Filter</div>
+      </div>
+      <div className="flex flex-col lg:flex-row  gap-4 p-2">
         <Autocomplete
-     
-            label=" Cluster"
-            placeholder="Select an Event"
-            size="sm"
-            className=""
-            onInputChange={searchClusters}
+          label=" Cluster"
+          placeholder="Select an Event"
+          size="sm"
+          className=""
+          onInputChange={searchClusters}
         >
-            {clusterList.map((cluster) => (
-                <AutocompleteItem value={cluster.cluster_name} key={cluster.cluster_ulid}>
-                    {cluster.cluster_name}
-                </AutocompleteItem>
-            ))}
-            
+          {clusterList.map((cluster) => (
+            <AutocompleteItem
+              value={cluster.cluster_name}
+              key={cluster.cluster_ulid}
+            >
+              {cluster.cluster_name}
+            </AutocompleteItem>
+          ))}
         </Autocomplete>
         <Autocomplete
-            label=" Event"
-            placeholder="Select an Event"
-            size="sm"
-            className=""
-            onInputChange={searchEvents}
+          label=" Event"
+          placeholder="Select an Event"
+          size="sm"
+          className=""
+          onInputChange={searchEvents}
         >
-            {eventList.map((event) => (
-                <AutocompleteItem value={event.event_name} key={event.event_ulid}>
-                    {event.event_name}
-                </AutocompleteItem>
-            ))}
+          {eventList.map((event) => (
+            <AutocompleteItem value={event.event_name} key={event.event_ulid}>
+              {event.event_name}
+            </AutocompleteItem>
+          ))}
         </Autocomplete>
         <div className="flex items-center gap-2">
-        <MyButton 
-            className="bg-green-400"
-            size="md"
-        >
+          <MyButton className="bg-green-400" size="md">
             <span className="text-black text-md font-medium">
-           Approve certificates
+              Approve certificates
             </span>
-            </MyButton>
-            <MyButton
-           className="bg-red-400"
-            size="md"
-        >
+          </MyButton>
+          <MyButton className="bg-red-400" size="md">
             <span className="dark:text-black text-white text-md font-medium">
-            reject certificates
+              reject certificates
             </span>
-            </MyButton>
-            <MyButton
-            className="bg-black dark:bg-white"
-            size="md"
-        >
+          </MyButton>
+          <MyButton className="bg-black dark:bg-white" size="md">
             <span className="dark:text-black text-white text-md font-medium">
-         Select All
+              Select All
             </span>
-            </MyButton>
-            </div>
-
-         
+          </MyButton>
         </div>
-        
       </div>
+    </div>
   );
 };
 
