871fec6f32674a06d421357780fe40b3572ceda2
AllenBastian <reachallenbastian@gmail.com>
2024-10-03 21:13:01 +0530

    ADDED settings security

--- src/types/global.types.tsx
+++ src/types/global.types.tsx
@@ -129,6 +129,13 @@ export interface issuedCertificatesType {
 
 //settings
 
+
+export interface MemberSettingType {
+  organization_name: string;
+  two_fa_enabled: boolean;
+  brand_logo_enabled: boolean;
+}
+
 export interface OrganizationSettingType {
   organization_name: string;
   organization_ulid: string;
