3d7306c1e2ab9f346fe0c6d48b4ee406c7759b0e
AllenBastian <reachallenbastian@gmail.com>
2025-01-14 19:27:47 +0530

    added: besu integrated draft version

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
 export default function Page() {
+
+    //credaegis-besu-integrated
     return (
         <div className="bg-gradient-radial from-[#000] to-[#333] h-screen flex items-center justify-center">
         
