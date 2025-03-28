94c2b5eb4bb64942211ad593f50a5e06d70bd651
AllenBastian <reachallenbastian@gmail.com>
2025-03-28 09:48:33 +0530

    fix: nav

--- src/components/pageComponents/certificates/organization/fileManagement/myFileList.tsx
+++ src/components/pageComponents/certificates/organization/fileManagement/myFileList.tsx
@@ -13,6 +13,7 @@ import { FileInfo, filesMetaType, MyFileType } from "@/types/global.types";
 import { ulid } from "ulid";
 import { Spinner } from "@nextui-org/react";
 import { EventSearchInfoType } from "@/types/event.types";
+import {GrFormUpload} from "react-icons/gr";
 
 interface MyFileListProps {
   loading: boolean;
@@ -193,7 +194,7 @@ const MyFileList: React.FC<MyFileListProps> = ({
           }}
         >
           <span className="dark:text-black text-white text-md font-medium">
-            Choose files
+            <GrFormUpload className="text-xl dark:text-black text-white" />
           </span>
         </MyButton>
       </div>
